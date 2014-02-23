01b9b5614d2c4277d3284085caaf1ce113c18fb4
matthewmilitante <matthewmilitante@gmail.com>
2014-02-22 19:01:02 -0500

    added search.py and edited begin.py to export the error message to a text file

--- begin.py
+++ begin.py
@@ -1,9 +1,31 @@
-import subprocess;
-import sys;
-command = '';
+import subprocess
+import sys
+import re
+
+command = ''
+
 for cmd in sys.argv[1:]:
-	command=command+' '+cmd;
-p = subprocess.Popen(command,shell=True,
-		stderr=subprocess.PIPE);
+	command=command+' '+cmd
+
+p = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE)
 out, err = p.communicate()
-print err;
+# write to the text file
+file = open('errors.txt', 'w')
+file.write(err)
+
+# open the file and read it
+file = open('errors.txt', 'r')
+inputList = file.read().split()
+#Add a bunch of 'junk patterns' here in which we will need to remove
+pattern1  = re.compile ("\"\w+\.\w+\"") #This pattern should match file names
+pattern2 = re.compile ("\w+\.\w+")
+
+print "After Parsing"
+for i in range(len(inputList)):
+	if re.match(pattern1,inputList[i]):
+		inputList[i] = ""
+	elif re.match(pattern2,inputList[i]):
+		inputList[i] = ""
+
+for j in inputList:
+	print j
